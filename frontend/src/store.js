546cba6fe9a9e70fbe69aada522d9e884d340c62
nmLeeki <leeki1501@gmail.com>
2023-04-10 18:17:20 +0900

    frontend/MainChats/수정팝업컴포넌트 추가 frontend/이미지이름 수정

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,72 +1,71 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-// user 상태 
+// user 상태
 const user = createSlice({
-    name : 'user', //state 이름
-    initialState : {
-      id:'',
-      email:'',
-      name:'',
-      aors: '',
-      nowLogInState : false,
+    name: 'user', //state 이름
+    initialState: {
+        id: '',
+        email: '',
+        name: '',
+        aors: '',
+        nowLogInState: false,
     }, //state 값
-    reducers : { 
-      updateUserInfoTrue(state, action){ 
-        return {...action.payload , nowLogInState:true}
-      },
-      updateUserInfoFalse(){
-        return {id:'', email:'', name:'', aors:'', nowLogInState:false}
-      }
-    }
-  })
-
-  //글로벌 알람 팝업 상태
-  const globalPop = createSlice({
-    name : 'globalPop',
-    initialState : {
-      nowPopState: false,
-      nowMessage : '알림입니다.',
+    reducers: {
+        updateUserInfoTrue(state, action) {
+            return { ...action.payload, nowLogInState: true };
+        },
+        updateUserInfoFalse() {
+            return { id: '', email: '', name: '', aors: '', nowLogInState: false };
+        },
     },
-    reducers :{
-      openPop(state, action){
-        state.nowPopState = true;
-        state.nowMessage = action.payload;
-      },
-      closePop(state, action){
-        state.nowPopState = false
-      }
+});
 
-    }
-  })
-
-  //글로벌 알람 팝업 상태
-  const globalSnackBar = createSlice({
-    name : 'globalSnackBar',
-    initialState : {
-      nowSnackBarState: false,
-      nowMessage : '알림입니다.',
+//글로벌 알람 팝업 상태
+const globalPop = createSlice({
+    name: 'globalPop',
+    initialState: {
+        nowPopState: false,
+        nowMessage: '알림입니다.',
     },
-    reducers :{
-      openSnackBar(state, action){
-        state.nowSnackBarState = true;
-        state.nowMessage = action.payload;
-      },
-      closeSnackBar(state, action){
-        state.nowSnackBarState = false;
-      }
-    }
-  })
+    reducers: {
+        openPop(state, action) {
+            state.nowPopState = true;
+            state.nowMessage = action.payload;
+        },
+        closePop(state, action) {
+            state.nowPopState = false;
+        },
+    },
+});
+
+//글로벌 알람 팝업 상태
+const globalSnackBar = createSlice({
+    name: 'globalSnackBar',
+    initialState: {
+        nowSnackBarState: false,
+        nowMessage: '알림입니다.',
+    },
+    reducers: {
+        openSnackBar(state, action) {
+            state.nowSnackBarState = true;
+            state.nowMessage = action.payload;
+        },
+        closeSnackBar(state, action) {
+            state.nowSnackBarState = false;
+        },
+    },
+});
 
 //reducers actions export
-export const { updateUserInfoTrue, updateUserInfoFalse } = user.actions 
-export const { openPop, closePop } = globalPop.actions
-export const { openSnackBar, closeSnackBar } = globalSnackBar.actions
+export const { updateUserInfoTrue, updateUserInfoFalse } = user.actions;
+export const { openPop, closePop } = globalPop.actions;
+export const { openSnackBar, closeSnackBar } = globalSnackBar.actions;
 
 //state 등록하기
-export default configureStore({ 
-  reducer: {
-    user : user.reducer, //작명 : createSlice만든거.reducer
-    globalPop : globalPop.reducer,
-    globalSnackBar : globalSnackBar.reducer
-  }
-})
+export default configureStore({
+    reducer: {
+        user: user.reducer, //작명 : createSlice만든거.reducer
+        globalPop: globalPop.reducer,
+        globalSnackBar: globalSnackBar.reducer,
+    },
+});
